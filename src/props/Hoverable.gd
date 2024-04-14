871e39e056d6f000c5c40bfb0b68497dd3eadcfd
MinaLbc <48332228+MinaLbc@users.noreply.github.com>
2024-04-14 18:53:41 +0200

    ingredients sounds added

--- src/props/Hoverable.gd
+++ src/props/Hoverable.gd
@@ -9,6 +9,7 @@ var is_idle: bool = true
 onready var _visuals: Node2D = $Visuals
 onready var _area_2d: Area2D = $Area2D
 onready var _highlight = $Visuals/Highlight
+onready var _sound = $sound
 
 var _is_hovered: bool = false
 
@@ -24,6 +25,7 @@ func _process(_delta) -> void:
 	
 	if Input.is_action_just_pressed("left_click") and _is_hovered:
 		emit_signal("selected", num)
+		_sound.play()
 
 
 func _on_Area2D_mouse_entered() -> void:
